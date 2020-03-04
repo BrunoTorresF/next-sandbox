6eb42a7f2a7905f870cb6cc0cf572b47f9c815a8
BrunoTorresF <bruno.torres.fabila@gmail.com>
2020-03-03 23:43:33 -0600

    feat(header): update header

--- components/Header.js
+++ components/Header.js
@@ -1,58 +1,78 @@
 import Link from 'next/link';
 
 const Header = () => {
-  return (
-    <header className="header" >
-      <div className="navigation">
-        <div>
-          <img />
-        </div>
-        <nav>
-          <Link href="/contact">
-            <a>Contact Us</a>
-          </Link>
-          <Link href="/guests">
-            <a>Guests</a>
-          </Link>
-        </nav>
-      </div>
-      <div className="actions">
-        <button className="header-cta subtle">
-          Lang
-        </button>
-        <button className="header-cta primary">
-          Login
-        </button>
-      </div>
-      <style jsx>{`
-    .header {
-      border: 1px solid blue;
-      display: grid;
-      grid-template-columns: 3fr 2fr;
-      column-gap: 75vw;
-    }
+	return (
+		<header className="header">
+			<div className="navigation">
+				<figure className="header-logo">
+					<img src="https://i.picsum.photos/id/1025/50/50.jpg" />
+				</figure>
+				<nav className="header-nav">
+					<Link href="/contact">
+						<a className="nav-item">Contact Us</a>
+					</Link>
+					<Link href="/guests">
+						<a className="nav-item">Guests</a>
+					</Link>
+				</nav>
+			</div>
+			<div className="actions">
+				<button className="btn-cta subtle">Lang</button>
+				<button className="btn-cta primary">Login</button>
+			</div>
+			<style jsx>{`
+				.header {
+					border: 1px solid blue;
+					display: grid;
+					grid-template-columns: repeat(6, auto);
+					align-items: center;
+					padding: 0 3.25em;
+				}
 
-    .navigation {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-evenly;
-    }
+				.navigation {
+					display: flex;
+					align-items: center;
+				}
 
-    .actions {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-evenly;
-    }
+				.header-logo {
+					margin-right: 2em;
+				}
 
-    .header-cta {
-      width: auto;
-      padding: 0.9375em 1.5625em;
-      margin-bottom: inherit;
-    }
+				.header-nav {
+					display: flex;
+				}
 
-  `}</style>
-    </header>
-  )
-}
+				.nav-item {
+					text-decoration: none;
+					color: inherit;
+				}
+
+				.nav-item:nth-of-type(odd):not(:last-of-type) {
+					margin-right: 2em;
+				}
+
+				.actions {
+					display: flex;
+					justify-content: flex-end;
+					grid-column-start: 6;
+					grid-column-end: 7;
+					padding-right: 1.625em;
+				}
+
+				.btn-cta {
+					padding: 0.9375em 1.5625em;
+					color: teal;
+					border: 1px solid teal;
+					border-radius: 5px;
+					background-color: transparent;
+				}
+
+				.btn-cta:nth-of-type(1) {
+					margin-right: 0.75em;
+				}
+			`}</style>
+		</header>
+	);
+};
 
 export default Header;
